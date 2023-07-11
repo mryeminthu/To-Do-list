df9caf8cd4df3906b8c3349b7f281ac96a8c2855
mryeminthu <dhammaarchive@hotmail.com>
2023-07-11 12:07:41 -0700

    Add html,css and js for to-do-list

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-
-function component() {
-    const element = document.createElement('div');
+const lists = [
+    { description: 'Wash the dishes', completed: false, index: 1 },
+    { description: 'Complete to-do list project', completed: false, index: 2 },
+  ];
   
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-    return element;
-  }
+  const provideLists = () => {
+    const placeholder = document.querySelector('.todo-placeholder');
+    placeholder.innerHTML = '';
+    const sortedLists = lists.sort((a, b) => a.index - b.index);
+    sortedLists.forEach((list) => {
+      const listItem = document.createElement('div');
+      listItem.className = 'todo';
+      listItem.innerHTML = `
+        <div>
+          <input type="checkbox" class="list" ${list.completed ? 'checked' : ''} />${list.description}
+        </div>
+        <i class="fa-solid fa-ellipsis-vertical"></i>
+      `;
+      placeholder.appendChild(listItem);
+    });
+  };
+  window.addEventListener('DOMContentLoaded', provideLists);
   
-  document.body.appendChild(component());
